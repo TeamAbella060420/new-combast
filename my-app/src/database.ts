d8a6c1e4a48d18038524dd55aa6c532486bed6b1
TeamAbella060420 <jabella@rscm.com.sa>
2023-02-21 00:10:19 +0800

    Create database.ts

---
+++ my-app/src/database.ts
@@ -0,0 +1,57 @@
+export const database = [
+    {
+      "TasNo": "Tas001234T",
+      "comments": [
+      { 
+        "DateTime": "10/13/2022 10:15", 
+        "CustomerName": "Wix", 
+        "ChangesText": "Request to issue", 
+        "CommentsText": "Some text of comment" 
+      }
+      ]
+    },
+    {
+      "TasNo": "Tas001235T",
+      "comments": [
+      {
+        "DateTime": "10/14/2022 13:35", 
+ 	 	 	  "CustomerName": "Wix", 
+ 	 	 	  "ChangesText": "Request to proposal", 
+ 	 	 	  "CommentsText": "Some text of comment" 
+      },
+      ]
+    },
+    {
+      "TasNo": "Tas000034T",
+      "comments": [
+        {
+          "DateTime": "09/13/2022 09:35", 
+          "CustomerName": "Playtika", 
+          "ChangesText": "Request to issue", 
+          "CommentsText": "Some text of comment" 
+        }
+      ]
+    },
+    {
+      "TasNo": "Tas000004T",
+      "comments": [
+        {
+          "DateTime": "09/20/2022 08:15", 
+ 	 	 	    "CustomerName": "GCash", 
+ 	 	 	    "ChangesText": "Request to issue", 
+ 	 	 	    "CommentsText": "Some text of comment" 
+        },
+      ]
+    },
+    {
+      "TasNo": "Tas000045T",
+      "comments": [
+        {
+          "DateTime": "10/01/2022 08:50", 
+          "CustomerName": "Adama", 
+          "ChangesText": "new Email", 
+          "CommentsText": "Some text of comment" 
+        },
+      ]
+    }
+  ]
